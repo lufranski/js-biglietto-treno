b09f09e95e22f8cee865f3a37d71d40cdd8f85af
lufranski <lufranski@gmail.com>
2022-11-07 14:09:59 +0100

    Macro JS

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Calcolo del prezzo del biglietto del treno
+
+// Variabili
+
+// Chiedo all'user il numero di chilometri da percorrere
+
+// Chiedo all'user la sua età
+
+// Calcolo del prezzo del viaggio
+
+// Output
